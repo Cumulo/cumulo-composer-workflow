37c000e04496faee5a2fc0811c663156c1c6a93f
jiyinyiyong <jiyinyiyong@gmail.com>
2019-05-19 15:49:59 +0800

    refactor based on work of brookress; remove deprecated components

--- src/app/vm.cljs
+++ src/app/vm.cljs
@@ -1,24 +1,66 @@
 
 (ns app.vm (:require [app.config :as config]))
 
-(defn get-view-model [store local-store]
-  {:local local-store, :site config/site, :store store})
+(defn get-view-model [store states] {:states states, :site config/site, :store store})
 
-(defn on-action [d! op param options view-model]
-  (when config/dev? (println "Action" op param (pr-str options)))
-  (case op
-    :local/username (d! :local-mutate {:path [:username], :value (:value options)})
-    :local/password (d! :local-mutate {:path [:password], :value (:value options)})
-    :signup
-      (let [local (:local view-model), pair [(:username local) (:password local)]]
-        (d! :user/sign-up pair)
-        (.setItem js/localStorage (:storage-key config/site) pair))
-    :login
-      (let [local (:local view-model), pair [(:username local) (:password local)]]
-        (d! :user/log-in pair)
-        (.setItem js/localStorage (:storage-key config/site) pair))
-    :logout
-      (do (d! :user/log-out nil) (.removeItem js/localStorage (:storage-key config/site)))
-    :profile (d! :router/change {:name :profile})
-    :home (d! :router/change {:name :home})
-    (do (println "Unknown op:" op))))
+(def state-header
+  {:init (fn [props state] state),
+   :update (fn [d! op context options state m!]
+     (case op
+       :home (d! :router/change {:name :home})
+       :profile (d! :router/change {:name :profile})
+       (println "Unhandled op:" op)))})
+
+(def state-login
+  {:init (fn [props state] (or state {:username "", :password ""})),
+   :update (fn [d! op context options state mutate!]
+     (case op
+       :username (mutate! (assoc state :username (:value options)))
+       :password (mutate! (assoc state :password (:value options)))
+       :signup
+         (let [login-pair [(:username state) (:password state)]]
+           (d! :user/sign-up login-pair)
+           (.setItem js/localStorage (:storage-key config/site) login-pair))
+       :login
+         (let [login-pair [(:username state) (:password state)]]
+           (d! :user/log-in login-pair)
+           (.setItem js/localStorage (:storage-key config/site) login-pair))
+       (println "Unhandled op:" op)))})
+
+(def state-offline
+  {:init (fn [props state] state),
+   :update (fn [d! op context options state m!]
+     (case op :reconnect (d! :effect/connect nil) (println "not handled op" op)))})
+
+(def state-profile
+  {:init (fn [props state] state),
+   :update (fn [d! op context options state m!]
+     (case op
+       :logout
+         (do (d! :user/log-out nil) (.removeItem js/localStorage (:storage-key config/site)))
+       (println "Unhandled op" op)))})
+
+(def state-workspace
+  {:init (fn [props state] state),
+   :update (fn [d! op context options state mutate!] (case op (println "Unhandled op" op)))})
+
+(def states-manager
+  {"offline" state-offline,
+   "login" state-login,
+   "workspace" state-workspace,
+   "profile" state-profile,
+   "header" state-header})
+
+(defn on-action [d! op context options view-model states]
+  (when config/dev? (println "Action" op context (pr-str options)))
+  (let [param (:param options)
+        template-name (:template-name context)
+        state-path (:state-path context)
+        mutate! (fn [x] (d! :states [state-path x]))
+        this-state (get-in states (conj state-path :data))]
+    (if (contains? states-manager template-name)
+      (let [action-handler (get-in states-manager [template-name :update])
+            state-fn (get-in states-manager [template-name :init])
+            state (if (fn? state-fn) (state-fn (:data context) this-state) this-state)]
+        (action-handler d! op context options state mutate!))
+      (println "Unhandled template:" template-name))))
